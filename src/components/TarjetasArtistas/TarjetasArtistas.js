c0478183d628ab5f83baac51378d7e2ec546d88d
matiasdiezz <mdiez@udesa.edu.ar>
2021-09-20 10:44:46 -0300

    Cambio tarjetasArtistas a funcnion

--- src/components/TarjetasArtistas/TarjetasArtistas.js
+++ src/components/TarjetasArtistas/TarjetasArtistas.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Tarjeta from '../Tarjetas/Tarjetas';
 import './TarjetasArtistas'
 
-class Artistas extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      
-    };
-
-  }
-
-  
-  render() {
+function Artistas (props){
 return (
       <>
         <section className='container-cards'>
           <article className="row align">
-          {this.props.cargando === false ? 
+          {props.cargando === false ? 
             <div className='loader'>
             <i className='fas fa-spinner fa-spin'></i>
             </div>
            : 
-            this.props.artistas.map((artista, i) => (
+            props.artistas.map((artista, i) => (
               <Tarjeta
-                changeOrder={this.props.changeOrder}
+                changeOrder={props.changeOrder}
                 key={artista.id + i}
                 datosArtista={artista}
-                borrarTarjeta= {(id)=>this.props.borrarTarjeta(id)}
+                borrarTarjeta= {(id)=>props.borrarTarjeta(id)}
               />
             ))
           }
           </article>
            {/* botón para cargar más artistas */}
           <button className="cargarArtistas" onClick={(Artistas) =>
-                  this.props.agregarArtistas(Artistas) }>
+                  props.agregarArtistas(Artistas) }>
             <i className="fas fa-chevron-down"></i>
           </button>
         </section>
       </>
     );
   }
-}
+
 
 
 
